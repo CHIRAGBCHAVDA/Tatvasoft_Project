4de48bf8cef572ca4d31e2a78321b2b9f3ef1cc6
chiragbchavda <chiragchavda0036@gmail.com>
2023-02-27 18:25:11 +0530

    Implemented architecture till 2 min video 2

---
+++ LoginPage/CIPlatform.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIPlatform.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        //T-user
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter);
+        void Register(T entity);
+
+    }
+}
---
+++ LoginPage/CIPlatform.DataAccess/Repository/Repository.cs
@@ -0,0 +1,28 @@
+﻿using CIPlatform.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIPlatform.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public readonly CiplatformContext _db;
+        public Repository(CiplatformContext db)
+        {
+            _db = db;
+        }
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
